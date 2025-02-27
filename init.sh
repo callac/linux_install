62a160a7d3fd5c21535a5291efade23cab6aedef
Eric <53767968@qq.com>
2025-02-27 15:24:48 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -348,9 +348,9 @@ install_docker()
     ca-certificates \
     curl \
     software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    curl -fsSL https://mirrors.aliyun.com/docker-ce/linux/ubuntu/gpg | sudo apt-key add -
     add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+     "deb [arch=amd64] https://mirrors.aliyun.com/docker-ce/linux/ubuntu \
      $(lsb_release -cs) \
      stable"
     apt-get update
